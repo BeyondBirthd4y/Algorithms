2921dd1f3ba7d8f7037b5d6735c1dd45d1f15a5f
Gabriel Fernandes <72037743+BeyondBirthd4y@users.noreply.github.com>
2022-10-11 00:51:15 -0300

    Add files via upload

---
+++ HorasPerfeitas/HorasPerfeitas/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HorasPerfeitas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t1 = new TimeSpan(0, 0, 0);
+            while (t1.Days < 1)
+            {
+                TimeSpan t2 = TimeSpan.FromSeconds(60);
+                t1 = t1.Add(t2);
+                if((t1.Hours/10 < t1.Hours%10) && (t1.Hours%10 < t1.Minutes/10) && (t1.Minutes/10 < t1.Minutes%10)){
+                    if((t1.Hours%10 == (t1.Hours/10 + 1)) && (t1.Minutes/10 == (t1.Hours%10 + 1)) && (t1.Minutes%10 == (t1.Minutes/10 + 1))){
+                        Console.WriteLine(t1);
+                    }
+                    
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
